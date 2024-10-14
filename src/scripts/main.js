864e2aeb8ad7af7799f18b155f6e50c8ed30e24e
Stanislav Kohut <stako211@gmail.com>
2024-10-14 13:06:45 +0300

    added task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,18 @@
 'use strict';
 
-// write your code here
+const table = document.querySelector('table');
+
+for (let i = 0; i < table.rows.length; i++) {
+  const row = table.rows[i];
+  const cells = row.cells[1].innerHTML;
+  const newCells = row.insertCell(row.cells.length - 1);
+
+  if (
+    row.parentNode.tagName === 'THEAD' ||
+    row.parentNode.tagName === 'TFOOT'
+  ) {
+    newCells.outerHTML = `<th>${cells}</th>`;
+  } else {
+    newCells.outerHTML = `<td>${cells}</td>`;
+  }
+}
